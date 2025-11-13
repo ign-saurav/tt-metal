d30b2262469c3b3b668737417e117aba848558d5
ign-krishnanand <krishnanand.k@ignitarium.com>
2025-11-13 13:17:34 +0000

    pick config and tokenizer from reference folder

--- models/experimental/miniCPMo/tests/test_mini_cpm_o.py
+++ models/experimental/miniCPMo/tests/test_mini_cpm_o.py
@@ -1,32 +1,37 @@
 # import ttnn
+import json
 import torch
 from models.experimental.miniCPMo.reference.modeling_minicpmo import MiniCPMO
+from models.experimental.miniCPMo.reference.configuration_minicpm import MiniCPMOConfig
 
-from transformers import AutoConfig
 from accelerate import init_empty_weights, load_checkpoint_and_dispatch
-from transformers import AutoTokenizer
 from PIL import Image
+from models.experimental.miniCPMo.reference.tokenization_minicpmo_fast import MiniCPMOTokenizerFast
+
+# from transformers import AutoTokenizer
 
 
 def test_mini_cpm_o():
-    config = AutoConfig.from_pretrained(
-        "openbmb/MiniCPM-o-2_6",
-        trust_remote_code=True,
+    # Load config directly from local JSON file
+    config_path = "models/experimental/miniCPMo/reference/config.json"
+    with open(config_path, "r") as f:
+        config_dict = json.load(f)
+
+    config = MiniCPMOConfig.from_dict(
+        config_dict,
         init_vision=True,
         init_audio=False,
         init_tts=False,
     )
 
-    # Set the model name/path (required for processor loading)
-    config._name_or_path = "openbmb/MiniCPM-o-2_6"
-    print(config)
     print("Initializing MiniCPM-o model...")
     # Initialize the model directly with the config
     # with torch.device("meta"):
     with init_empty_weights():
         model = MiniCPMO(config)
 
-    local_checkpoint_path = "/home/ubuntu/.cache/huggingface/hub/models--openbmb--MiniCPM-o-2_6/snapshots/509805e84db1c84f154034d71a21c4f2331e6e11"
+    # local_checkpoint_path = "/home/ubuntu/.cache/huggingface/hub/models--openbmb--MiniCPM-o-2_6/snapshots/509805e84db1c84f154034d71a21c4f2331e6e11"
+    local_checkpoint_path = "models/experimental/miniCPMo/reference/safetensors"
     load_checkpoint_and_dispatch(
         model,
         local_checkpoint_path,
@@ -36,7 +41,13 @@ def test_mini_cpm_o():
     # Set model to eval mode
     model = model.eval()
 
-    tokenizer = AutoTokenizer.from_pretrained("openbmb/MiniCPM-o-2_6", trust_remote_code=True)
+    # tokenizer = AutoTokenizer.from_pretrained("openbmb/MiniCPM-o-2_6", trust_remote_code=True)
+
+    # Load tokenizer from local reference folder
+    tokenizer_path = "models/experimental/miniCPMo/reference"
+    tokenizer = MiniCPMOTokenizerFast.from_pretrained(tokenizer_path)
+
+    # input Image and question
     image = Image.open("cat_img.jpg").convert("RGB")
     question = "What is in the image?"
     msgs = [{"role": "user", "content": [image, question]}]
