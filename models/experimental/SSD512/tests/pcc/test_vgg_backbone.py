94f70ca0e3a9d32b847bd4758492cd4eeda243cd
Mayank Sati <mayank.sati@ignitarium.com>
2025-12-18 17:50:30 +0000

    added tt-cnn to vgg backbone

--- models/experimental/SSD512/tests/pcc/test_vgg_backbone.py
+++ models/experimental/SSD512/tests/pcc/test_vgg_backbone.py
@@ -8,65 +8,161 @@ import pytest
 import ttnn
 from loguru import logger
 
-from models.experimental.SSD512.common import SSD512_L1_SMALL_SIZE
-from models.experimental.SSD512.reference.ssd import vgg, base
-from models.experimental.SSD512.tt.layers.tt_vgg_backbone import build_vgg_backbone, apply_vgg_backbone
+# from models.experimental.SSD512.common import SSD512_L1_SMALL_SIZE
+# from models.experimental.SSD512.reference.ssd import vgg
+# from models.experimental.SSD512.tt.layers.tt_vgg_backbone import build_vgg_backbone, apply_vgg_backbone
+from models.experimental.SSD512.tt.layers.tt_vgg_backbone import TtVGGBackbone
 from models.common.utility_functions import comp_pcc
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
+from models.tt_cnn.tt.builder import (
+    Conv2dConfiguration,
+    MaxPool2dConfiguration,
+)
+from models.tt_cnn.tt.builder import (
+    Conv2dConfiguration,
+    MaxPool2dConfiguration,
+    AutoShardedStrategyConfiguration,
+    L1FullSliceStrategyConfiguration,
+)
+
+# SSD512_L1_SMALL_SIZE = 98304
+SSD512_L1_SMALL_SIZE = 2457
+
+
+# This function is derived from torchvision VGG make_layers()
+# https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+def vgg(cfg, i=3, batch_norm=False):
+    layers = []
+    in_channels = i
+    for v in cfg:
+        if v == "M":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif v == "C":
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    pool5 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+    conv6 = nn.Conv2d(512, 1024, kernel_size=3, padding=6, dilation=6)
+    conv7 = nn.Conv2d(1024, 1024, kernel_size=1)
+    layers += [pool5, conv6, nn.ReLU(inplace=True), conv7, nn.ReLU(inplace=True)]
+    return layers
+
 
 @pytest.mark.parametrize("pcc", ((0.99),))
+# @pytest.mark.parametrize("size", ((256,)))
 @pytest.mark.parametrize("size", ((512,)))
-@pytest.mark.parametrize("device_params", [{"l1_small_size": SSD512_L1_SMALL_SIZE}], indirect=True)
+# @pytest.mark.parametrize("device_params", [{"l1_small_size": SSD512_L1_SMALL_SIZE}], indirect=True)
 def test_vgg_backbone(device, pcc, size, reset_seeds):
+    base = {
+        "300": [64, 64, "M", 128, 128, "M", 256, 256, 256, "C", 512, 512, 512, "M", 512, 512, 512],
+        "512": [64, 64, "M", 128, 128, "M", 256, 256, 256, "C", 512, 512, 512, "M", 512, 512, 512],
+    }
+    batch_size = 1
+    input_channels = 3
+    torch_input = torch.randn(batch_size, input_channels, size, size)
+    ttnn_input_tensor = ttnn.from_torch(
+        torch_input.permute(0, 2, 3, 1), layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat16, device=device
+    )
     cfg = base[str(size)]
     torch_layers = vgg(cfg, i=3, batch_norm=False)
     torch_model = nn.Sequential(*torch_layers)
     torch_model.eval()
+    # Export the torch_model to ONNX
+    # import os
+    # from ttnn.torch_tracer import trace, visualize
+    # visualize(torch_model, file_name="model_graph.svg")
+    # onnx_filename = "vgg_backbone.onnx"
+    # torch.onnx.export(
+    #     torch_model,
+    #     torch_input,
+    #     onnx_filename)
+    #     export_params=True,
+    #     opset_version=12,
+    #     do_constant_folding=True,
+    #     # input_names=['input'],
+    #     # output_names=['output'],
+    #     # dynamic_axes={
+    #     #     'input': {0: 'batch_size'},
+    #     #     'output': {0: 'batch_size'}
+    #     # },
+    # )
+    # print(f"Exported VGG backbone torch model to {onnx_filename}")
+    # model_config = {
+    #     "math_fidelity": ttnn.MathFidelity.LoFi,
+    #     "weights_dtype": ttnn.bfloat8_b,
+    #     "activation_dtype": ttnn.bfloat8_b,
+    #     "output_dtype": ttnn.bfloat8_b,
+    #     "deallocate_activation":True,
+    #     "sharding_strategy":WidthSliceStrategyConfiguration
+    # }
 
-    batch_size = 1
-    input_channels = 3
-    torch_input = torch.randn(batch_size, input_channels, size, size)
+    model_config = {
+        "weights_dtype": ttnn.bfloat8_b,
+        "output_dtype": ttnn.bfloat8_b,
+        "activation_dtype": ttnn.bfloat8_b,
+        "sharding_strategy": AutoShardedStrategyConfiguration(),
+        "slice_strategy": L1FullSliceStrategyConfiguration(),
+        "math_fidelity": ttnn.MathFidelity.LoFi,
+        "fp32_dest_acc_en": True,
+        "packer_l1_acc": False,
+        "deallocate_activation": True,
+        "enable_act_double_buffer": True,
+        "enable_weights_double_buffer": False,
+        "reallocate_halo_output": True,
+        "activation": ttnn.UnaryWithParam(ttnn.UnaryOpType.RELU),
+        "config_tensors_in_dram": True,
+    }
+
+    #############################################33
+    # torch_model = nn.ModuleList(torch_layers)
+    conv_config_layers = []
+    with torch.no_grad():
+        x = torch_input
+        for i, layer in enumerate(torch_model):
+            print(layer.__class__.__name__, x.shape)
+            # Create Conv2dConfiguration from the current layer, given torch input height, width, batch_size
+            if isinstance(layer, nn.Conv2d):
+                conv_config_layers.append(
+                    Conv2dConfiguration.from_torch(
+                        layer,
+                        input_height=x.shape[-2],
+                        input_width=x.shape[-1],
+                        batch_size=x.shape[0],
+                        **model_config,
+                    )
+                )
+            elif isinstance(layer, nn.MaxPool2d):
+                conv_config_layers.append(
+                    MaxPool2dConfiguration.from_torch(
+                        layer,
+                        input_height=x.shape[-2],
+                        input_width=x.shape[-1],
+                        channels=x.shape[-3],
+                        batch_size=x.shape[0],
+                    )
+                )
+
+            # x = torch.nn.functional.relu(layer(x), inplace=True)
+            x = layer(x)
+        torch_output = x
+    ########################################################
 
     with torch.no_grad():
         torch_output = torch_model(torch_input)
 
-    layers_config = build_vgg_backbone(size=size, input_channels=input_channels, device=device)
-    torch_conv_idx = 0
-    layers_with_weights = []
-
-    for layer in layers_config:
-        if layer["type"] == "conv":
-            while torch_conv_idx < len(torch_model):
-                torch_layer = torch_model[torch_conv_idx]
-                if isinstance(torch_layer, nn.Conv2d):
-                    break
-                torch_conv_idx += 1
-
-            torch_conv = torch_model[torch_conv_idx]
-            torch_conv_idx += 1
-
-            weight = torch_conv.weight.data.clone()
-            bias = torch_conv.bias.data.clone() if torch_conv.bias is not None else None
-
-            weight_ttnn = ttnn.from_torch(weight, device=device, dtype=ttnn.bfloat16, layout=ttnn.ROW_MAJOR_LAYOUT)
-            bias_ttnn = None
-            if bias is not None:
-                bias_reshaped = bias.reshape((1, 1, 1, -1))
-                bias_ttnn = ttnn.from_torch(
-                    bias_reshaped, device=device, dtype=ttnn.bfloat16, layout=ttnn.ROW_MAJOR_LAYOUT
-                )
-
-            layer_with_weights = layer.copy()
-            layer_with_weights["weight"] = weight_ttnn
-            layer_with_weights["bias"] = bias_ttnn
-            layers_with_weights.append(layer_with_weights)
-        else:
-            layers_with_weights.append(layer.copy())
-
-    tt_output_ttnn = apply_vgg_backbone(
-        torch_input, layers_with_weights, device=device, dtype=ttnn.bfloat8_b, memory_config=ttnn.DRAM_MEMORY_CONFIG
+    tt_vgg_backbone = TtVGGBackbone(
+        conv_config_layer=conv_config_layers,
+        batch_size=batch_size,
+        device=device,
     )
+
+    tt_output_ttnn = tt_vgg_backbone(device, ttnn_input_tensor)
     tt_output = ttnn.to_torch(tt_output_ttnn)
 
     if len(tt_output.shape) == 4:
@@ -76,3 +172,13 @@ def test_vgg_backbone(device, pcc, size, reset_seeds):
     _, pcc_message = comp_pcc(torch_output, tt_output, pcc)
     logger.info(f"VGG Backbone PCC: {pcc_message}")
     assert_with_pcc(torch_output, tt_output, pcc)
+
+
+# def create_conv_block_config(in_channels, out_channels, input_size, batch_size, device):
+#     """Create a typical CNN block: Conv -> Conv -> MaxPool"""
+
+#     input_height, input_width = input_size
+#     sharding_strategy = HeightShardedStrategyConfiguration(
+#         act_block_h_override=64,
+#         reshard_if_not_optimal=False
+#     )
--- models/experimental/SSD512/tt/utils.py
+++ models/experimental/SSD512/tt/utils.py
@@ -4,110 +4,117 @@
 import ttnn
 from models.tt_cnn.tt.builder import (
     Conv2dConfiguration,
-    AutoShardedStrategyConfiguration,
-    L1FullSliceStrategyConfiguration,
+)
+from models.tt_cnn.tt.builder import (
+    Conv2dConfiguration,
+    MaxPool2dConfiguration,
 )
 
-
+# conv_config = {
+#     "MATH_FIDELITY": ttnn.MathFidelity.HiFi4,
+#     "WEIGHTS_DTYPE": ttnn.bfloat16,
+#     "ACTIVATIONS_DTYPE": ttnn.bfloat16,
+# }
 conv_config = {
-    "MATH_FIDELITY": ttnn.MathFidelity.HiFi4,
-    "WEIGHTS_DTYPE": ttnn.bfloat16,
-    "ACTIVATIONS_DTYPE": ttnn.bfloat16,
+    "math_fidelity": ttnn.MathFidelity.LoFi,
+    "weights_dtype": ttnn.bfloat8_b,
+    "activation_dtype": ttnn.bfloat8_b,
 }
 
 
-def _create_conv_config_from_params(
-    input_height: int,
-    input_width: int,
-    in_channels: int,
-    out_channels: int,
-    batch_size: int,
-    parameters: dict,
-    device,
-    kernel_size=(1, 1),
-    stride=(1, 1),
-    padding=(0, 0),
-    dilation=(1, 1),
-    groups=1,
-    activation=None,
-    deallocate_activation=False,
-    activation_dtype=None,
-    weights_dtype=None,
-    output_dtype=None,
-    math_fidelity=None,
-    sharding_strategy=AutoShardedStrategyConfiguration(),
-) -> Conv2dConfiguration:
-    weight = parameters["weight"]
-    bias = parameters.get("bias")
-
-    if not isinstance(weight, ttnn.Tensor):
-        weight_ttnn, bias_ttnn = Conv2dConfiguration.convert_torch_weight_and_bias_to_ttnn(weight, bias)
-        if device is not None:
-            weight_ttnn = ttnn.to_device(weight_ttnn, device)
-            if bias_ttnn is not None:
-                bias_ttnn = ttnn.to_device(bias_ttnn, device)
-    else:
-        weight_ttnn = weight
-        bias_ttnn = bias
-
-    return Conv2dConfiguration(
-        input_height=input_height,
-        input_width=input_width,
-        in_channels=in_channels,
-        out_channels=out_channels,
-        batch_size=batch_size,
-        kernel_size=kernel_size,
-        stride=stride,
-        padding=padding,
-        groups=groups,
-        dilation=dilation,
-        weight=weight_ttnn,
-        bias=bias_ttnn,
-        activation=activation,
-        activation_dtype=activation_dtype or conv_config["ACTIVATIONS_DTYPE"],
-        weights_dtype=weights_dtype or conv_config["WEIGHTS_DTYPE"],
-        output_dtype=output_dtype or conv_config["ACTIVATIONS_DTYPE"],
-        math_fidelity=math_fidelity or conv_config["MATH_FIDELITY"],
-        sharding_strategy=sharding_strategy,
-        slice_strategy=L1FullSliceStrategyConfiguration(),
-        enable_act_double_buffer=True,
-        enable_weights_double_buffer=True,
-        deallocate_activation=deallocate_activation,
-        reallocate_halo_output=True,
-    )
+def create_config_layers(torch_model, torch_input, model_config=conv_config):
+    conv_config_layers = []
+    # with torch.no_grad():
+    x = torch_input
+    for i, layer in enumerate(torch_model):
+        print(layer.__class__.__name__, x.shape)
+        # Create Conv2dConfiguration from the current layer, given torch input height, width, batch_size
+        if isinstance(layer, nn.Conv2d):
+            conv_config_layers.append(
+                Conv2dConfiguration.from_torch(
+                    layer,
+                    input_height=x.shape[-2],
+                    input_width=x.shape[-1],
+                    batch_size=x.shape[0],
+                    **model_config,
+                )
+            )
+        elif isinstance(layer, nn.MaxPool2d):
+            conv_config_layers.append(
+                MaxPool2dConfiguration.from_torch(
+                    layer,
+                    input_height=x.shape[-2],
+                    input_width=x.shape[-1],
+                    channels=x.shape[-3],
+                    batch_size=x.shape[0],
+                    # **dtype= ttnne.bfloat8_b
+                )
+            )
+        # x = torch.nn.functional.relu(layer(x), inplace=True)
+        x = layer(x)
+    torch_output = x
+    return conv_config_layers, torch_output
 
 
-# def extract_extras_parameters_from_torch(torch_model):
-#     parameters = []
-#     for torch_layer in torch_model:
-#         weight = torch_layer.weight.data
-#         bias = torch_layer.bias.data if torch_layer.bias is not None else None
-#         parameters.append({"weight": weight, "bias": bias})
-#     return parameters
+# def _create_conv_config_from_params(
+#     input_height: int,
+#     input_width: int,
+#     in_channels: int,
+#     out_channels: int,
+#     batch_size: int,
+#     parameters: dict,
+#     device,
+#     kernel_size=(1, 1),
+#     stride=(1, 1),
+#     padding=(0, 0),
+#     dilation=(1, 1),
+#     groups=1,
+#     activation=None,
+#     deallocate_activation=False,
+#     activation_dtype=None,
+#     weights_dtype=None,
+#     output_dtype=None,
+#     math_fidelity=None,
+#     sharding_strategy=AutoShardedStrategyConfiguration(),
+# ) -> Conv2dConfiguration:
+#     weight = parameters["weight"]
+#     bias = parameters.get("bias")
 
-# def extract_extras_parameters_from_torch(torch_model):
-#     parameters = []
-#     for torch_layer in torch_model:
-#         layer_params = {
-#             "weight": torch_layer.weight.data,
-#             "bias": torch_layer.bias.data if torch_layer.bias is not None else None,
-#         }
+#     if not isinstance(weight, ttnn.Tensor):
+#         weight_ttnn, bias_ttnn = Conv2dConfiguration.convert_torch_weight_and_bias_to_ttnn(weight, bias)
+#         if device is not None:
+#             weight_ttnn = ttnn.to_device(weight_ttnn, device)
+#             if bias_ttnn is not None:
+#                 bias_ttnn = ttnn.to_device(bias_ttnn, device)
+#     else:
+#         weight_ttnn = weight
+#         bias_ttnn = bias
 
-#         # Store conv metadata if this is a Conv2d
-#         if isinstance(torch_layer, torch.nn.Conv2d):
-#             layer_params.update(
-#                 {
-#                     "kernel_size": torch_layer.kernel_size,  # e.g. (3, 3)
-#                     "stride": torch_layer.stride,            # e.g. (1, 1)
-#                     "padding": torch_layer.padding,          # e.g. (1, 1)
-#                     "dilation": torch_layer.dilation,
-#                     "groups": torch_layer.groups,
-#                 }
-#             )
-
-#         parameters.append(layer_params)
-
-#     return parameters
+#     return Conv2dConfiguration(
+#         input_height=input_height,
+#         input_width=input_width,
+#         in_channels=in_channels,
+#         out_channels=out_channels,
+#         batch_size=batch_size,
+#         kernel_size=kernel_size,
+#         stride=stride,
+#         padding=padding,
+#         groups=groups,
+#         dilation=dilation,
+#         weight=weight_ttnn,
+#         bias=bias_ttnn,
+#         activation=activation,
+#         activation_dtype=activation_dtype or conv_config["ACTIVATIONS_DTYPE"],
+#         weights_dtype=weights_dtype or conv_config["WEIGHTS_DTYPE"],
+#         output_dtype=output_dtype or conv_config["ACTIVATIONS_DTYPE"],
+#         math_fidelity=math_fidelity or conv_config["MATH_FIDELITY"],
+#         sharding_strategy=sharding_strategy,
+#         slice_strategy=L1FullSliceStrategyConfiguration(),
+#         enable_act_double_buffer=True,
+#         enable_weights_double_buffer=True,
+#         deallocate_activation=deallocate_activation,
+#         reallocate_halo_output=True,
+#     )
 
 
 import torch.nn as nn
@@ -161,53 +168,53 @@ def extract_extras_parameters_from_torch(torch_model, input_height, input_width,
     return parameters
 
 
-# Helper function to create Conv2dConfiguration from parameters
-def _create_conv_config_from_params(
-    input_height: int,
-    input_width: int,
-    in_channels: int,
-    out_channels: int,
-    batch_size: int,
-    parameters: dict,
-    kernel_size=(1, 1),
-    stride=(1, 1),
-    padding=(0, 0),
-    dilation=(1, 1),
-    groups=1,
-    activation=None,
-    deallocate_activation=False,
-    activation_dtype=None,
-    weights_dtype=None,
-    output_dtype=None,
-    math_fidelity=None,
-    sharding_strategy=AutoShardedStrategyConfiguration(),
-) -> Conv2dConfiguration:
-    """
-    Conv2dConfiguration from parameters dict for SqueezeExcitation.
-    """
+# # Helper function to create Conv2dConfiguration from parameters
+# def _create_conv_config_from_params(
+#     input_height: int,
+#     input_width: int,
+#     in_channels: int,
+#     out_channels: int,
+#     batch_size: int,
+#     parameters: dict,
+#     kernel_size=(1, 1),
+#     stride=(1, 1),
+#     padding=(0, 0),
+#     dilation=(1, 1),
+#     groups=1,
+#     activation=None,
+#     deallocate_activation=False,
+#     activation_dtype=None,
+#     weights_dtype=None,
+#     output_dtype=None,
+#     math_fidelity=None,
+#     sharding_strategy=AutoShardedStrategyConfiguration(),
+# ) -> Conv2dConfiguration:
+#     """
+#     Conv2dConfiguration from parameters dict for SqueezeExcitation.
+#     """
 
-    return Conv2dConfiguration(
-        input_height=input_height,
-        input_width=input_width,
-        in_channels=in_channels,
-        out_channels=out_channels,
-        batch_size=batch_size,
-        kernel_size=kernel_size,
-        stride=stride,
-        padding=padding,
-        groups=groups,
-        dilation=dilation,
-        weight=parameters["weight"],
-        bias=parameters["bias"],
-        activation=activation,
-        activation_dtype=activation_dtype or conv_config["ACTIVATIONS_DTYPE"],
-        weights_dtype=weights_dtype or conv_config["WEIGHTS_DTYPE"],
-        output_dtype=output_dtype or conv_config["ACTIVATIONS_DTYPE"],
-        math_fidelity=math_fidelity or conv_config["MATH_FIDELITY"],
-        sharding_strategy=sharding_strategy,
-        slice_strategy=L1FullSliceStrategyConfiguration(),
-        enable_act_double_buffer=True,
-        enable_weights_double_buffer=True,
-        deallocate_activation=deallocate_activation,
-        reallocate_halo_output=True,
-    )
+#     return Conv2dConfiguration(
+#         input_height=input_height,
+#         input_width=input_width,
+#         in_channels=in_channels,
+#         out_channels=out_channels,
+#         batch_size=batch_size,
+#         kernel_size=kernel_size,
+#         stride=stride,
+#         padding=padding,
+#         groups=groups,
+#         dilation=dilation,
+#         weight=parameters["weight"],
+#         bias=parameters["bias"],
+#         activation=activation,
+#         activation_dtype=activation_dtype or conv_config["ACTIVATIONS_DTYPE"],
+#         weights_dtype=weights_dtype or conv_config["WEIGHTS_DTYPE"],
+#         output_dtype=output_dtype or conv_config["ACTIVATIONS_DTYPE"],
+#         math_fidelity=math_fidelity or conv_config["MATH_FIDELITY"],
+#         sharding_strategy=sharding_strategy,
+#         slice_strategy=L1FullSliceStrategyConfiguration(),
+#         enable_act_double_buffer=True,
+#         enable_weights_double_buffer=True,
+#         deallocate_activation=deallocate_activation,
+#         reallocate_halo_output=True,
+#     )
