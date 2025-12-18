ee5486b506d60c6bfcdfd35d9959bc71b4610883
ign-saurav <sauravkumar.mishra@ignitarium.com>
2025-12-18 10:06:51 +0000

    code refactoring - extrasbackbone1

--- models/experimental/SSD512/tests/pcc/test_extras_backbone.py
+++ models/experimental/SSD512/tests/pcc/test_extras_backbone.py
@@ -8,9 +8,12 @@ import ttnn
 from loguru import logger
 
 from models.experimental.SSD512.reference.ssd import add_extras, extras
-from models.experimental.SSD512.tt.layers.tt_extras_backbone import build_extras_backbone
 from models.common.utility_functions import comp_pcc
 from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.SSD512.tt.utils import extract_extras_parameters_from_torch
+from models.experimental.SSD512.tt.layers.tt_extras_backbone import (
+    TtExtrasBackbone,
+)
 
 
 @pytest.mark.parametrize("pcc", ((0.99),))
@@ -35,20 +38,20 @@ def test_extras_backbone(device, pcc, size, reset_seeds):
             x = torch.nn.functional.relu(layer(x), inplace=True)
         torch_output = x
 
-    tt_extras = build_extras_backbone(size=size, input_channels=input_channels, batch_size=batch_size, device=device)
-    tt_extras.load_weights_from_torch(torch_model)
+    parameters = extract_extras_parameters_from_torch(torch_model)
+
+    tt_extras = TtExtrasBackbone(
+        size=size, input_channels=input_channels, batch_size=batch_size, parameters=parameters, device=device
+    )
 
     tt_output_ttnn = tt_extras(torch_input)
     tt_output = ttnn.to_torch(tt_output_ttnn)
 
-    # Ensure correct shape before permutation
     expected_shape = torch_output.shape
     if tt_output.shape != (expected_shape[0], expected_shape[2], expected_shape[3], expected_shape[1]):
-        # Reshape from flattened or incorrect shape to NHWC
         B, C, H, W = expected_shape
         tt_output = tt_output.reshape(B, H, W, C)
 
-    # Convert NHWC to NCHW
     if len(tt_output.shape) == 4:
         tt_output = tt_output.permute(0, 3, 1, 2)
     tt_output = tt_output.float()
@@ -58,6 +61,7 @@ def test_extras_backbone(device, pcc, size, reset_seeds):
         min_shape = [min(s1, s2) for s1, s2 in zip(torch_output.shape, tt_output.shape)]
         torch_output = torch_output[tuple(slice(0, s) for s in min_shape)]
         tt_output = tt_output[tuple(slice(0, s) for s in min_shape)]
+
     _, pcc_message = comp_pcc(torch_output, tt_output, pcc)
     logger.info(f"Extras Backbone PCC: {pcc_message}")
     assert_with_pcc(torch_output, tt_output, pcc)
