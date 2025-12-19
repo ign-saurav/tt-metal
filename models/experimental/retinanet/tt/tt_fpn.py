74976884f792d8b3d089932b06406c0a0d9a8aa2
Febin Sebastian <febin.sebastian@ignitarium.com>
2025-12-19 08:34:10 +0000

    fpn ported to tt cnn

--- models/experimental/retinanet/tt/tt_fpn.py
+++ models/experimental/retinanet/tt/tt_fpn.py
@@ -100,9 +100,8 @@ class resnet50Fpn:
         model_args,
         layer_optimisations=fpn_optimisations,
     ) -> None:
-        print(model_args)
         self.conv_config_1 = Conv2dConfiguration.from_model_args(
-            model_args["conv1"],
+            model_args["inner_blocks"][0]["inner_blocks"][0][0],
             weights=parameters["inner_blocks"].get("0", {}).get("0", None)["weight"],
             bias=parameters["inner_blocks"].get("0", {}).get("0", None)["bias"],
             # **layer_optimisations.conv1,
@@ -113,7 +112,7 @@ class resnet50Fpn:
         self.conv1 = TtConv2d(self.conv_config_1, device)
 
         self.conv_config_2 = Conv2dConfiguration.from_model_args(
-            model_args["conv1"],
+            model_args["inner_blocks"][1]["inner_blocks"][1][0],
             weights=parameters["inner_blocks"].get("1", {}).get("0", None)["weight"],
             bias=parameters["inner_blocks"].get("1", {}).get("0", None)["bias"],
             # **layer_optimisations.conv2,
@@ -124,7 +123,7 @@ class resnet50Fpn:
         self.conv2 = TtConv2d(self.conv_config_2, device)
 
         self.conv_config_3 = Conv2dConfiguration.from_model_args(
-            model_args["conv1"],
+            model_args["inner_blocks"][2]["inner_blocks"][2][0],
             weights=parameters["inner_blocks"].get("2", {}).get("0", None)["weight"],
             bias=parameters["inner_blocks"].get("2", {}).get("0", None)["bias"],
             # **layer_optimisations.conv3,
@@ -135,7 +134,7 @@ class resnet50Fpn:
         self.conv3 = TtConv2d(self.conv_config_3, device)
 
         self.conv_config_4 = Conv2dConfiguration.from_model_args(
-            model_args["conv1"],
+            model_args["layer_blocks"][0]["layer_blocks"][0][0],
             weights=parameters["layer_blocks"].get("0", {}).get("0", None)["weight"],
             bias=parameters["layer_blocks"].get("0", {}).get("0", None)["bias"],
             # **layer_optimisations.conv4,
@@ -146,7 +145,7 @@ class resnet50Fpn:
         self.conv4 = TtConv2d(self.conv_config_4, device)
 
         self.conv_config_5 = Conv2dConfiguration.from_model_args(
-            model_args["conv1"],
+            model_args["layer_blocks"][1]["layer_blocks"][1][0],
             weights=parameters["layer_blocks"].get("1", {}).get("0", None)["weight"],
             bias=parameters["layer_blocks"].get("1", {}).get("0", None)["bias"],
             # **layer_optimisations.conv5,
@@ -157,7 +156,7 @@ class resnet50Fpn:
         self.conv5 = TtConv2d(self.conv_config_5, device)
 
         self.conv_config_6 = Conv2dConfiguration.from_model_args(
-            model_args["conv1"],
+            model_args["layer_blocks"][2]["layer_blocks"][2][0],
             weights=parameters["layer_blocks"].get("2", {}).get("0", None)["weight"],
             bias=parameters["layer_blocks"].get("2", {}).get("0", None)["bias"],
             # **layer_optimisations.conv6,
@@ -168,7 +167,7 @@ class resnet50Fpn:
         self.conv6 = TtConv2d(self.conv_config_6, device)
 
         self.conv_config_7 = Conv2dConfiguration.from_model_args(
-            model_args["conv1"],
+            model_args["extra_blocks"]["extra_blocks"]["p6"],
             weights=getattr(parameters.extra_blocks, "p6", None)["weight"],
             bias=getattr(parameters.extra_blocks, "p6", None)["bias"],
             # **layer_optimisations.conv7,
@@ -179,7 +178,7 @@ class resnet50Fpn:
         self.conv7 = TtConv2d(self.conv_config_7, device)
 
         self.conv_config_8 = Conv2dConfiguration.from_model_args(
-            model_args["conv1"],
+            model_args["extra_blocks"]["extra_blocks"]["p7"],
             weights=getattr(parameters.extra_blocks, "p7", None)["weight"],
             bias=getattr(parameters.extra_blocks, "p7", None)["bias"],
             # **layer_optimisations.conv8,
@@ -218,10 +217,12 @@ class resnet50Fpn:
         L5, [_out_height, _out_width] = self.conv3(C5, return_output_dim=True)
 
         P5 = L5
-        P5_interpolated = self.upsample1(device, P5, P5.shape, reshape_output=False, sent_to_dram=False)
+        P5_shape = (1, _out_height, _out_width, 256)
+        P5_interpolated = self.upsample1(device, P5, P5_shape, reshape_output=True, sent_to_dram=False)
 
         P4 = ttnn.add(L4, P5_interpolated)
-        P4_interpolated = self.upsample1(device, P4, P4.shape, reshape_output=False, sent_to_dram=False)
+        P4_shape = (1, 32, 32, 256)
+        P4_interpolated = self.upsample1(device, P4, P4_shape, reshape_output=True, sent_to_dram=False)
 
         P3 = ttnn.add(L3, P4_interpolated)
 
