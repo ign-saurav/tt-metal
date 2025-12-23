c2c1ddeb2aac261a81533cfdc3737055b4ecf6f1
ign-saurav <sauravkumar.mishra@ignitarium.com>
2025-12-23 20:17:38 +0000

    petr funcional with TT CCNN architecture

--- models/experimental/petr/test/pcc/test_cp_fpn.py
+++ models/experimental/petr/test/pcc/test_cp_fpn.py
@@ -1,4 +1,5 @@
-# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
 # SPDX-License-Identifier: Apache-2.0
 
 import ttnn
@@ -7,27 +8,52 @@ import pytest
 from ttnn.model_preprocessing import preprocess_model_parameters
 from tests.ttnn.utils_for_testing import assert_with_pcc, check_with_pcc
 from loguru import logger
-from models.experimental.petr.tt.ttnn_cp_fpn import ttnn_CPFPN
+from models.experimental.petr.tt.tt_cp_fpn import ttnn_CPFPN
 from models.experimental.petr.reference.cp_fpn import CPFPN
-from models.experimental.petr.tt.common import create_custom_preprocessor_cpfpn
+from models.experimental.petr.tt.model_preprocessing import (
+    create_custom_preprocessor_cpfpn,
+    infer_ttnn_module_args_cp_fpn,
+)
+
+
+model_config = {
+    "MATH_FIDELITY": ttnn.MathFidelity.HiFi4,
+    "WEIGHTS_DTYPE": ttnn.bfloat16,
+    "ACTIVATIONS_DTYPE": ttnn.bfloat16,
+}
 
 
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 16384}], indirect=True)
 def test_cp_fpn(device, reset_seeds):
     torch_model = CPFPN(in_channels=[768, 1024], out_channels=256, num_outs=2)
     torch_model.eval()
+
     parameters = preprocess_model_parameters(
         initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor_cpfpn(None), device=None
     )
 
-    input_a = torch.randn(6, 768, 20, 50)
-    input_b = torch.randn(6, 1024, 10, 25)
+    batch_size = 6
+    input_a = torch.randn(batch_size, 768, 20, 50)
+    input_b = torch.randn(batch_size, 1024, 10, 25)
     torch_output = torch_model([input_a, input_b])
-    ttnn_model = ttnn_CPFPN(in_channels=[768, 1024], out_channels=256, num_outs=2, parameters=parameters)
+    ttnn_module_args = infer_ttnn_module_args_cp_fpn(
+        model=torch_model, run_model=lambda model: model([input_a, input_b]), device=device
+    )
+
+    ttnn_model = ttnn_CPFPN(
+        in_channels=[768, 1024],
+        out_channels=256,
+        num_outs=2,
+        batch_size=batch_size,
+        parameters=parameters,
+        model_config=model_config,
+        model_args=ttnn_module_args,
+        device=device,
+    )
 
     ttnn_input_1 = ttnn.from_torch(input_a.permute(0, 2, 3, 1), layout=ttnn.TILE_LAYOUT, device=device)
     ttnn_input_2 = ttnn.from_torch(input_b.permute(0, 2, 3, 1), layout=ttnn.TILE_LAYOUT, device=device)
-    ttnn_output = ttnn_model(device, [ttnn_input_1, ttnn_input_2])
+    ttnn_output = ttnn_model([ttnn_input_1, ttnn_input_2])
 
     for i in range(len(ttnn_output)):
         ttnn_output_check = ttnn.to_torch(ttnn_output[i])
--- models/experimental/petr/tt/tt_petr.py
+++ models/experimental/petr/tt/tt_petr.py
@@ -1,12 +1,13 @@
-# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
 import ttnn
-from models.experimental.petr.tt.ttnn_petr_head import ttnn_PETRHead
-from models.experimental.petr.tt.ttnn_vovnetcp import ttnn_VoVNetCP
-from models.experimental.petr.tt.ttnn_cp_fpn import ttnn_CPFPN
-from models.experimental.petr.tt.ttnn_grid_mask import ttnn_GridMask
+from models.experimental.petr.tt.tt_petr_head import ttnn_PETRHead
+from models.experimental.petr.tt.tt_vovnetcp import ttnn_VoVNetCP
+from models.experimental.petr.tt.tt_cp_fpn import ttnn_CPFPN
+from models.experimental.petr.tt.tt_grid_mask import ttnn_GridMask
 from models.experimental.petr.reference.utils import bbox3d2result
 
 
@@ -19,6 +20,7 @@ class ttnn_PETR:
         parameters=None,
         query_embedding_input=None,
         device=None,
+        model_config=None,
     ):
         self.with_img_neck = True
         self.pts_bbox_head = ttnn_PETRHead(
@@ -36,8 +38,21 @@ class ttnn_PETR:
         self.img_backbone = ttnn_VoVNetCP(
             parameters=parameters["img_backbone"], stem_parameters=parameters["stem_parameters"], device=device
         )
+        if model_config is None:
+            model_config = {
+                "MATH_FIDELITY": ttnn.MathFidelity.HiFi4,
+                "WEIGHTS_DTYPE": ttnn.bfloat16,
+                "ACTIVATIONS_DTYPE": ttnn.bfloat16,
+            }
         self.img_neck = ttnn_CPFPN(
-            in_channels=[768, 1024], out_channels=256, num_outs=2, parameters=parameters["img_neck"]
+            in_channels=[768, 1024],
+            out_channels=256,
+            num_outs=2,
+            batch_size=1,
+            parameters=parameters["img_neck"],
+            model_config=model_config,
+            model_args=None,
+            device=device,
         )
         self.grid_mask = ttnn_GridMask(True, True, rotate=1, offset=False, ratio=0.5, mode=1, prob=0.7)
         self.use_grid_mask = use_grid_mask
@@ -87,7 +102,7 @@ class ttnn_PETR:
             return None
 
         if self.with_img_neck:
-            img_feats = self.img_neck(device=self.device, inputs=img_feats)
+            img_feats = self.img_neck(inputs=img_feats)
 
         img_feats_reshaped = []
         for img_feat in img_feats:
