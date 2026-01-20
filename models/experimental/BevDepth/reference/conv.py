20bf6f2956f3463a3d65b9b0a922310993ae055b
ign-saurav <sauravkumar.mishra@ignitarium.com>
2026-01-20 14:37:49 +0000

    license year updated

--- models/experimental/BevDepth/reference/conv.py
+++ models/experimental/BevDepth/reference/conv.py
@@ -1,5 +1,6 @@
-# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC.
+# SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC.
 # SPDX-License-Identifier: Apache-2.0
+
 ########################################################
 # Adapted from: https://github.com/open-mmlab/mmcv/blob/main/mmcv/cnn/bricks/conv.py
 # Copyright (c) OpenMMLab. All rights reserved.
